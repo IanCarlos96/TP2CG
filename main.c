01786e638c79920974987c5568217e23a77fade3
Samuel Viana <contato.santanasamuel@gmail.com>
2023-07-04 07:59:00 -0300

    add/SOIL

--- main.c
+++ main.c
@@ -223,7 +223,7 @@ static void atoa(void)
 // GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
 
 // configura alguns parâmetros do modelo de iluminação: MATERIAL
-GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
+GLfloat mat_ambient[]    = { 0.4f, 0.4f, 0.4f, 1.0f };
 GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
 GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
 GLfloat high_shininess[] = { 100.0f };
